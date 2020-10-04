b378bb007fc943be1c27986774e6cf21aa57a813
Miroslav <m.tetevenski1994@gmail.com>
2020-10-04 13:47:36 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        })
+    ]
+}
