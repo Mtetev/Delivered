2562995cf63dc1facf2f9b8c07f8fd3c91ee3840
Miroslav <m.tetevenski1994@gmail.com>
2020-10-12 15:39:09 +0300

    add riders photo and general fixes

--- src/Containers/CareersSection/CareersSection.js
+++ src/Containers/CareersSection/CareersSection.js
@@ -5,9 +5,15 @@ import WorkCard from "../../Components/WorkCard/WorkCard";
 const CareersSection = () => {
     return (
         <section className="section-careers">
-            <WorkCard id="1" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
-            <WorkCard id="2" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
-            <WorkCard id="3" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+            <div className="work-card__wrapper">
+                <WorkCard id="1" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+            </div>    
+            <div className="work-card__wrapper">
+                <WorkCard id="2" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+            </div>    
+            <div className="work-card__wrapper">
+                <WorkCard id="3" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+            </div>
         </section>
     )
 }
