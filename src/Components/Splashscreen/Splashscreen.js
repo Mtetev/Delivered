7f0c868fe7acf4548d8036f97d6fc21a3727d9a9
Miroslav <m.tetevenski1994@gmail.com>
2020-10-13 15:19:37 +0300

    fix Splashscreen content not being aligned

--- src/Components/Splashscreen/Splashscreen.js
+++ src/Components/Splashscreen/Splashscreen.js
@@ -4,13 +4,15 @@ import "./Splashscreen.scss";
 const Splashscreen = () => {
     return (
         <section className="splashscreen">
-            <p className="splashscreen__description">Your favorite restaurants, delivered to your home</p>
-            <form className="splashscreen-form">
-                <label for="splashscreen-form__input-field" className="splashscreen-form__label">Enter your adress to find local restaurants</label>
-                <input id="splashscreen-form__input-field" type="text" className="splashscreen-form__input-field" placeholder="Enter your address"/>
-                <input type="submit" className="splashscreen-form__button" value="Search"/>
-            </form>
-            <p className="splashscreen__ad">#Delivered to you!</p>
+            <div className="splashscreen__content">
+                <p className="splashscreen__description">Your favorite restaurants, delivered to your home</p>
+                <form className="splashscreen-form">
+                    <label for="splashscreen-form__input-field" className="splashscreen-form__label">Enter your adress to find local restaurants</label>
+                    <input id="splashscreen-form__input-field" type="text" className="splashscreen-form__input-field" placeholder="Enter your address"/>
+                    <input type="submit" className="splashscreen-form__button" value="Search"/>
+                </form>
+                <p className="splashscreen__ad">#Delivered to you!</p>
+            </div>
         </section>
     )
 }
