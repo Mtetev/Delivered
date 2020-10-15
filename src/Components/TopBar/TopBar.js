dfb368943cd58a5f62412990aa82e4d16bb07c36
Miroslav <m.tetevenski1994@gmail.com>
2020-10-15 13:55:06 +0300

    resize TopBar for desktop

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -4,14 +4,16 @@ import "./TopBar.scss";
 const TopBar = () => {
     return (
         <div className="topbar">
-            <div className="topbar-logo">
-                <ion-icon name="cube-outline" class="topbar-logo__icon"></ion-icon>
-                <h2 className="topbar-logo__name">delivered</h2>
-            </div>
-            <div className="topbar-actions">
-                <a href="#" className="topbar-actions__button">Log in </a>
-                <a href="#" className="topbar-actions__button">Partner with us</a>
-                <a href="#" className="topbar-actions__button">Menu</a>
+            <div className="topbar__content">
+                <div className="topbar-logo">
+                    <ion-icon name="cube-outline" class="topbar-logo__icon"></ion-icon>
+                    <h2 className="topbar-logo__name">delivered</h2>
+                </div>
+                <div className="topbar-actions">
+                    <a href="#" className="topbar-actions__button">Log in </a>
+                    <a href="#" className="topbar-actions__button">Partner with us</a>
+                    <a href="#" className="topbar-actions__button">Menu</a>
+                </div>
             </div>
         </div>
     )
