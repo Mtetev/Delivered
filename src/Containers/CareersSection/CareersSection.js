871d9fcfd83d8e2192e28687d9ab65b7147339a2
Miroslav <m.tetevenski1994@gmail.com>
2020-10-15 13:40:12 +0300

    add header to careers section

--- src/Containers/CareersSection/CareersSection.js
+++ src/Containers/CareersSection/CareersSection.js
@@ -6,14 +6,17 @@ const CareersSection = () => {
     return (
         <section className="careers-section">
             <div className="careers__content">
-                <div className="work-card__wrapper">
-                    <WorkCard id="1" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
-                </div>    
-                <div className="work-card__wrapper">
-                    <WorkCard id="2" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
-                </div>    
-                <div className="work-card__wrapper">
-                    <WorkCard id="3" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                <h3 className="careers__header">Work with Delivered</h3>
+                <div className="careers__cards">
+                    <div className="work-card__wrapper">
+                        <WorkCard id="1" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                    </div>    
+                    <div className="work-card__wrapper">
+                        <WorkCard id="2" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                    </div>    
+                    <div className="work-card__wrapper">
+                        <WorkCard id="3" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                    </div>
                 </div>
             </div>
         </section>
