2b2111f5f1b2f94c78bd841d790191db6c4356b7
Miroslav <m.tetevenski1994@gmail.com>
2020-10-15 13:52:40 +0300

    add different career descriptions

--- src/Containers/CareersSection/CareersSection.js
+++ src/Containers/CareersSection/CareersSection.js
@@ -9,13 +9,37 @@ const CareersSection = () => {
                 <h3 className="careers__header">Work with Delivered</h3>
                 <div className="careers__cards">
                     <div className="work-card__wrapper">
-                        <WorkCard id="1" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                        <WorkCard 
+                            id="1"
+                            img={{
+                                url: "../../../img/careers1.jpg",
+                                description: "Riders"
+                            }}
+                            title="Riders"
+                            description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts."
+                        />
                     </div>    
                     <div className="work-card__wrapper">
-                        <WorkCard id="2" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                        <WorkCard 
+                            id="2"
+                            img={{
+                                url: "../../../img/careers2.jpg",
+                                description: "Food"
+                            }}
+                            title="Restaurants"
+                            description="Partner with Deliveroo and reach more customers than ever. We handle delivery, so you can focus on the food."
+                        />
                     </div>    
                     <div className="work-card__wrapper">
-                        <WorkCard id="3" img={{url: "../../../img/bike.jpg", description: "Riders picture"}} title="Riders" description="Become a rider and enjoy the freedom to fit work around your life. Plus great fees, perks and discounts." />
+                        <WorkCard 
+                            id="3" 
+                            img={{
+                                url: "../../../img/careers3.jpg", 
+                                description: "Office workers"
+                            }}
+                            title="Careers"
+                            description="Our mission is to change the way people eat. It's ambitious, but so are we. And we need people like you to help us do it."
+                        />
                     </div>
                 </div>
             </div>
